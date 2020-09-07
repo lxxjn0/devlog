c0e3954b0975a10859a9507c685be1771bcaee2b
lxxjn0 <ljy3510@gmail.com>
2020-09-07 23:22:50 +0900

    refactor: 날짜 형식 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
+        formatString: "MM.DD.YYYY",
         navigation: [
           {
             title: `About`,
